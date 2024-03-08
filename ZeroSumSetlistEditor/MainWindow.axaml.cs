1a12c9779dc483a3152303b7cf3364b7ee77bb49
Ratamacue9112 <lito2001@icloud.com>
2024-03-08 21:54:56 +0000

    Initialize project

---
+++ ZeroSumSetlistEditor/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ZeroSumSetlistEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
