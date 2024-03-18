2848749fd50139f32a69e998cb77b35d83be929b
Ratamacue9112 <lito2001@icloud.com>
2024-03-18 18:07:51 +0000

    Add basic role edit view

---
+++ ZeroSumSetlistEditor/ViewModels/SongEdit/RoleEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroSumSetlistEditor.ViewModels 
+{
+    public class RoleEditViewModel : ViewModelBase
+    {
+        public string Artist { get; set; }
+        public ObservableCollection<string> Roles { get; set; }
+
+        public RoleEditViewModel(string artist, List<string> roles) 
+        {
+            Artist = artist;
+            Roles = new ObservableCollection<string>(roles);
+        }
+    }
+}
---
+++ ZeroSumSetlistEditor/Views/SongEdit/RoleEditView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ZeroSumSetlistEditor.Views
+{
+    public partial class RoleEditView : UserControl
+    {
+        public RoleEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
